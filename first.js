fdac6b971dcc627cb4bbdb221948ac8178f13e50
eric mwangi <nuthuerick147@gmail.com>
2024-03-25 17:27:51 +0300

    commented lines

--- first.js
+++ first.js
@@ -565,47 +565,47 @@ console.log(secondToLastLetterOfLastName);*/
 // var firstTree = myPlants[1].list[0];
 // console.log(firstTree);
 
-var collection = {
-    "2548" :{
-        "album" : "Slippery when wet",
-        "artist" : "Bon Jovi",
-        "tracks": [
-            "Let It Rock",
-            "You Give Love a Bad Name"
-        ]
-    },
-    "2468": {
-        "album": "1999",
-        "artist": "prince",
-        "tracks": [
-            "1999",
-            "Little Red Corvette"
-        ]
-    },
-    "1245": {
-        "artist" : "Robert Palmer",
-        "tracks" : [ ] 
-    },
-    "5439": {
-        "album": "ABBA Gold"
-    }
-};
+// var collection = {
+//     "2548" :{
+//         "album" : "Slippery when wet",
+//         "artist" : "Bon Jovi",
+//         "tracks": [
+//             "Let It Rock",
+//             "You Give Love a Bad Name"
+//         ]
+//     },
+//     "2468": {
+//         "album": "1999",
+//         "artist": "prince",
+//         "tracks": [
+//             "1999",
+//             "Little Red Corvette"
+//         ]
+//     },
+//     "1245": {
+//         "artist" : "Robert Palmer",
+//         "tracks" : [ ] 
+//     },
+//     "5439": {
+//         "album": "ABBA Gold"
+//     }
+// };
 
-var collectionCopy = JSON.parse(JSON.stringify(collection));
+// var collectionCopy = JSON.parse(JSON.stringify(collection));
 
-function updateRecords(id, prop, value){
-    if (value === ""){
-        delete collection [id][prop];
-    }else if(prop==="tracks"){
-        collection [id][prop] = collection [id][prop] || [];
-        collection [id][prop].push(value);
-    }else{
-        collection[id][prop]=value;
-    }
+// function updateRecords(id, prop, value){
+//     if (value === ""){
+//         delete collection [id][prop];
+//     }else if(prop==="tracks"){
+//         collection [id][prop] = collection [id][prop] || [];
+//         collection [id][prop].push(value);
+//     }else{
+//         collection[id][prop]=value;
+//     }
 
-    return collection;
-}
-console.log(updateRecords(5439, "artist", "ABBA"));
+//     return collection;
+// }
+// console.log(updateRecords(5439, "artist", "ABBA"));
 
 
 
